7bd91ce260896cbffef591bb5da5856845f3e8ae
Diana <dianaguerrero191997@gmail.com>
2026-06-21 19:09:00 -0400

    puli el codigo

--- Estudiante.cpp
+++ Estudiante.cpp
@@ -1,9 +1,20 @@
-#include "Estudiante.h" //incluyo la pantilla de la clase Estudiante
+#include "Estudiante.h" // incluyo la plantilla de la clase Estudiante
 #include <iostream>
 #include <fstream>
-// creacion objecto estudiante (instancia de la clase Estudiante) y definicion de sus metodos
-Estudiante::Estudiante(std::string _nombre, std::string _matricula, std::string _seccion) {
-    this ->nombre = _nombre;
-    this ->matricula = _matricula;
-    this ->seccion = _seccion;
+#include <string>
+
+// Constructor de la clase Estudiante
+Estudiante::Estudiante(std::string _nombre, std::string _materia, std::string _matricula, float _seccion) {
+    this->nombre = _nombre;
+    this->materia = _materia;
+    this->matricula = _matricula;
+    this->seccion = _seccion;
+}
+
+// Método para mostrar la información del estudiante
+void Estudiante::mostrar() {
+    std::cout << "Soy el estudiante: " << this->nombre
+              << ", mi materia es: " << this->materia
+              << ", mi matrícula es: " << this->matricula
+              << ", sección: " << this->seccion << std::endl;
 }
--- Estudiante.h
+++ Estudiante.h
@@ -1,22 +1,26 @@
 #pragma once
 #include <iostream>
-#include <vector>
+#include <string>
+
 // Esta es la plantilla de la clase Estudiante, que contiene los atributos y metodos de la clase.
 
 class Estudiante {
-    private: //Atributos privados de la clase Estudiante
+private: // Atributos privados de la clase Estudiante
     std::string nombre;
     std::string materia;
     std::string matricula;
     float seccion;
 
-    public: // constructores y metodos publicos de la clase Estudiante
-    Estudiante(std::string _nombre, std::string _matricula, float  _seccion);
+public: // Constructores y metodos publicos de la clase Estudiante
+    Estudiante(std::string _nombre, std::string _materia, std::string _matricula, float _seccion);
+
     // metodo para mostrar la informacion del estudiante
     void mostrar(); 
+
     std::string get_Nombre() { return nombre; } 
-    std::string get_nombre() { return materia;}
+    std::string get_Materia() { return materia; }
     std::string get_Matricula() { return matricula; } 
     float get_Seccion() { return seccion; } 
+
     void serializar(std::ofstream & archivo); 
 };
