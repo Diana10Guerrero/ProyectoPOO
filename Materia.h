6fb7c708ddb02b14a3b97f5252900a1a247191aa
Diana <dianaguerrero191997@gmail.com>
2026-06-21 19:48:11 -0400

    creando atrivutos y constructor

--- Materia.h
+++ Materia.h
@@ -2,3 +2,19 @@
 #pragma once
 #include <iostream>
 #include <fstream>
+
+class Materia{
+    private:
+        std::string nombre;
+        float codigo;
+        float creditos;
+// constructor de materia
+    public:
+     Materia(std::string _nombre, float _codigo, float _creditos);
+        void mostrar_info();
+        std:: string get_nombre() { return nombre; }
+        float get_codigo() { return codigo; }
+        float get_creditos() { return creditos; }
+        std::string serializar() {
+            return {"Nombre" + nombre + "\nCodigo:" + float _codigo + "\nCreditos:" + float _creditos}
+        };
