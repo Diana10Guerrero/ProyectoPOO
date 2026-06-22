99922ee6a2a808de5c65ff577b9d47e0f16d5b67
Diana <dianaguerrero191997@gmail.com>
2026-06-21 20:25:20 -0400

    calificaciones oara el estudiante

--- Materia.cpp
+++ Materia.cpp
@@ -2,3 +2,11 @@
 #include "Materia1.h"
 #include <iostream>
 #include <fstream>
+
+//defino constructor de materia
+Materia::Materia(std::string _nombre, float _codigo, float _creditos) {
+    this ->nombre = _nombre;
+    this ->profesor = _profesor
+    this ->codigo = _codigo;
+    this ->creditos = _creditos;
+}
