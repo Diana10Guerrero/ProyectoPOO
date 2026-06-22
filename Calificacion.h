e0d615d6cf70237a2b1eb330fbef1f70728c3f3e
Diana <dianaguerrero191997@gmail.com>
2026-06-21 20:39:43 -0400

    .

--- Calificacion.h
+++ Calificacion.h
@@ -1,5 +1,15 @@
 #pragma once
 #include <iostream>
-#include "Estudiante1.h"
+#include "Estudiant.h"
 #include "Materia.h"
 #include "Profesor1.h"
+
+class Calificacion {
+    private:
+        std::string estudiante;
+        std::string materia;
+        std::string profesor;
+        float nota;
+   
+    public:
+    s
