a05f94742f6ddad731f5e7e71e5bc6408d1060a7
Diana <dianaguerrero191997@gmail.com>
2026-06-21 20:03:25 -0400

    iniciando materia.cpp y modificando materia.h

--- Materia.h
+++ Materia.h
@@ -2,19 +2,30 @@
 #pragma once
 #include <iostream>
 #include <fstream>
+#include <string>
 
-class Materia{
-    private:
-        std::string nombre;
-        float codigo;
-        float creditos;
-// constructor de materia
-    public:
-     Materia(std::string _nombre, float _codigo, float _creditos);
-        void mostrar_info();
-        std:: string get_nombre() { return nombre; }
-        float get_codigo() { return codigo; }
-        float get_creditos() { return creditos; }
-        std::string serializar() {
-            return {"Nombre" + nombre + "\nCodigo:" + float _codigo + "\nCreditos:" + float _creditos}
-        };
+class Materia {
+private:
+    std::string nombre;
+    float codigo;
+    float creditos;
+
+public:
+    // Constructor
+    Materia(std::string _nombre, float _codigo, float _creditos);
+
+    // Mostrar información
+    void mostrar_info();
+
+    // Getters
+    std::string get_nombre() { return nombre; }
+    float get_codigo() { return codigo; }
+    float get_creditos() { return creditos; }
+
+    // Serializar como string
+    std::string serializar() {
+        return "Nombre: " + nombre +
+               "\nCodigo: " + std::to_string(codigo) +
+               "\nCreditos: " + std::to_string(creditos) + "\n";
+    }
+};
